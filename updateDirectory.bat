cea09a29d47beea00e1d6a716e1e57f30acc1ba4
Arthur Clay Odom <thekingofthebritons@gmail.com>
2022-02-08 14:34:04 -0500

    Removed pointless .bat, copy scripts not important

--- updateDirectory.bat
+++
@@ -1,5 +0,0 @@
-set "source=./"
-
-set "destination=./Portfolio/"
-
-robocopy "%source%" "%destination%" /XD ".git" Portfolio /s /XA:H /purge /XF ".gitignore .brackets.json:" ".notjekyll" "debug.log" "updateDirectory.bat"
